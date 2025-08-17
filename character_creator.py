9cc418b18cd55909518f6e527ecb17b801bcc131
Sam <usernamessuck10@gmail.com>
2025-08-17 01:00:49 -0700

    helper functions and stuff testing

---
+++ character_creator.py
@@ -0,0 +1,116 @@
+import pygame
+
+back_hairs = [
+    "character_images/Back Hair/Bat Wings .png",
+    "character_images/Back Hair/Butterfly Braids.png",
+    "character_images/Back Hair/Dreads Bob.png",
+    "character_images/Back Hair/Fluffy Long.png",
+    "character_images/Back Hair/Fluffy Short.png",
+    "character_images/Back Hair/Layer 89.png",
+    "character_images/Back Hair/Lob.png",
+    "character_images/Back Hair/Long Afro .png",
+    "character_images/Back Hair/Long Straight.png",
+    "character_images/Back Hair/Mullet.png",
+    "character_images/Back Hair/Short Afro .png",
+    "character_images/Back Hair/Short Afro.png",
+    "character_images/Back Hair/Short Wavy.png"
+]
+
+bangs = [
+    "character_images/Bangs/Afro Bangs.png",
+    "character_images/Bangs/Braid Bangs.png",
+    "character_images/Bangs/Curly Bangs.png",
+    "character_images/Bangs/Fluffed.png",
+    "character_images/Bangs/Harsh Side Part .png",
+    "character_images/Bangs/Middle Part.png",
+    "character_images/Bangs/Pixie.png",
+    "character_images/Bangs/Reverse V Bangs.png",
+    "character_images/Bangs/Sexy Bangs.png",
+    "character_images/Bangs/Sexy Dreads.png",
+    "character_images/Bangs/Side Part Bangs.png",
+    "character_images/Bangs/Slick Bangs.png",
+    "character_images/Bangs/Slick Dreads.png",
+    "character_images/Bangs/Split Bangs.png",
+    "character_images/Bangs/Straight Bangs.png",
+    "character_images/Bangs/V Bangs.png"
+]
+ponytails = [
+    "character_images/Ponytails/Braid L.png",
+    "character_images/Ponytails/Braid R.png",
+    "character_images/Ponytails/Double Buns .png",
+    "character_images/Ponytails/High Ponytail.png",
+    "character_images/Ponytails/L Side Pony.png",
+    "character_images/Ponytails/Layer 73.png",
+    "character_images/Ponytails/Low Ponytail.png",
+    "character_images/Ponytails/Puff .png",
+    "character_images/Ponytails/R Side Pony.png",
+    "character_images/Ponytails/Twin Puffs.png"
+]
+sides = [
+    "character_images/Sides/1 Braid Side .png",
+    "character_images/Sides/2 Braid Side.png",
+    "character_images/Sides/Fluffy Side.png",
+    "character_images/Sides/Hime Side.png",
+    "character_images/Sides/Long Dreads Side.png",
+    "character_images/Sides/Long Side.png",
+    "character_images/Sides/Medium Side.png",
+    "character_images/Sides/Rough Sides .png",
+    "character_images/Sides/Short Curls Side.png",
+    "character_images/Sides/Short Dreads Side .png"
+]
+brows = [
+    "character_images/Brows/Diva Brows .png",
+    "character_images/Brows/Fluffy Brows.png",
+    "character_images/Brows/Medium Brows.png",
+    "character_images/Brows/Puffy Eyebrows.png",
+    "character_images/Brows/Sad Brows.png",
+    "character_images/Brows/Straight Brows.png"
+]
+eyes = [
+    "character_images/Eyes/Cheerful Eyes.png",
+    "character_images/Eyes/Doe Eyes.png",
+    "character_images/Eyes/Serious Eyes.png",
+    "character_images/Eyes/Sexy Eyes.png"
+]
+face_accessories = [
+    "character_images/Face Accessories/Circle Glasses.png",
+    "character_images/Face Accessories/Freckles .png",
+    "character_images/Face Accessories/Scar 1.png",
+    "character_images/Face Accessories/Scar 2.png",
+    "character_images/Face Accessories/Wrinkles 1.png",
+    "character_images/Face Accessories/Wrinkles 2.png"
+]
+noses = [
+    "character_images/Noses/Beak Nose.png",
+    "character_images/Noses/Flat Nose .png",
+    "character_images/Noses/Hook Nose.png",
+    "character_images/Noses/Upturned Nose.png"
+]
+mouths = [
+    "character_images/Mouths/Big Frown.png",
+    "character_images/Mouths/Diva Lipstick.png",
+    "character_images/Mouths/Kitty Mouth.png",
+    "character_images/Mouths/Neutral Mouth.png",
+    "character_images/Mouths/Pouty Mouth.png"
+]
+
+
+
+class CharacterCreator:
+
+    def __init__(self):
+
+        self.back_hair = None
+        self.bangs = None
+        self.ponytail = None
+        self.sides = None
+
+        self.brows = None
+        self.eyes = None
+        self.face_accessories = None
+        self.nose = None
+        self.mouth = None
+
+
+    def Create_char(self):
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-import pygame 
+# Example file showing a circle moving on screen
+import pygame
+import helper_functions as hf
+
+# pygame setup
+pygame.init()
+screen_width = 1020
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+main_background = pygame.image.load("background_space.png").convert()
+tile_width, tile_height = main_background.get_size()
+
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    for x in range(0, screen_width, tile_width):
+        for y in range(0, screen_height, tile_height):
+            screen.blit(main_background, (x, y))
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+
+    hf.display_image(screen, "character_images/Back Hair/Bat Wings .png", (100, 100))
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
+
+
